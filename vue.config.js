708a65686915c819e1c3710eea34ae38b36aab9b
Parasitic-art-guild <plan.mantenimiento.industrial@gmail.com>
2023-11-25 21:36:12 -0500

    pruenas en vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,18 @@ module.exports = {
     // ...
     pages: {
         home: {
-            entry: 'src/main.js',
+            entry: './main.js',
             template: '/pages/home.html',
             filename: 'home.html',
         },
         login: {
-            entry: 'src/main.js',
-            template: '/pages/login.html',
+            entry: './main.js',
+            template: './pages/login.html',
             filename: 'login.html',
         },
         register: {
-            entry: '/main.js',
-            template: '/pages/register.html',
+            entry: './main.js',
+            template: './pages/register.html',
             filename: 'register.html',
         },
     }
