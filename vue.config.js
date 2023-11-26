45fcb4be531c75f6f34151ff8ca9746ae44b01b0
Parasitic-art-guild <plan.mantenimiento.industrial@gmail.com>
2023-11-25 21:47:56 -0500

    pruebas con vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,17 @@
-module.exports = {
-    // ...
-    pages: {
-        home: {
-            entry: './main.js',
-            template: '/pages/home.html',
-            filename: 'home.html',
-        },
-        login: {
-            entry: './main.js',
-            template: './pages/login.html',
-            filename: 'login.html',
-        },
-        register: {
-            entry: './main.js',
-            template: './pages/register.html',
-            filename: 'register.html',
-        },
-    }
-
+export const pages = {
+    home: {
+        entry: '/main.js',
+        template: '/pages/home.html',
+        filename: 'home.html',
+    },
+    login: {
+        entry: '/main.js',
+        template: '/pages/login.html',
+        filename: 'login.html',
+    },
+    register: {
+        entry: '/main.js',
+        template: '/pages/register.html',
+        filename: 'register.html',
+    },
 };
