c2871f42cb444349ad1ff2cd632ab61009fbb63f
Parasitic-art-guild <plan.mantenimiento.industrial@gmail.com>
2023-11-25 21:26:28 -0500

    Adicion de archivo vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    // ...
+    pages: {
+        home: {
+            entry: 'src/main.js',
+            template: '/pages/home.html',
+            filename: 'home.html',
+        },
+        login: {
+            entry: 'src/main.js',
+            template: '/pages/login.html',
+            filename: 'login.html',
+        },
+        register: {
+            entry: 'src/main.js',
+            template: '/pages/register.html',
+            filename: 'register.html',
+        },
+    }
+
+};
